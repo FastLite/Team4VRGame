3f6dd2fdb18cb7d9a4b90dd09bb62d3f68f4c611
Jaedyn mitchell <Jaedyn15mitchell@gmail.com>
2021-03-17 17:55:23 -0400

    player movement code

---
+++ Assets/Scripts/VrplayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class VrplayerMovement : MonoBehaviour
+{
+
+    public SteamVR_Action_Vector2 touchpadInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movementDir = Player.instance.hmdTransform.TransformDirection(new Vector3(touchpadInput.axis.x, 0, touchpadInput.axis.y));
+        transform.position += Vector3.ProjectOnPlane(Time.deltaTime * movementDir * 2f, Vector3.up);
+    }
+}
+
