3f1669706f36e93a77933794c19996ba4266eb35
Vladislav Volodin (FastLite) <Volodin_vlad2002@mail.ru>
2021-04-07 18:08:29 +0300

    Whiteboard, Fan animation, bug fixes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,22 +5,13 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-
-    
     void Awake()
     {
         if (instance !=null & instance !=this)
         {
             Destroy(this.gameObject);
         }
-
         instance = this;
-
-
-       
         DontDestroyOnLoad(gameObject);
     }
-    
-   
-    
 }
